c3034e62ceb5c738f3b8e8db749db1fbc3dc36ce
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2022-06-28 09:19:36 -0300

    Aula 02 - Referência, ponteiro e métodos

--- banco/main.go
+++ banco/main.go
@@ -9,21 +9,26 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
-	contaCorrente1 := ContaCorrente{
-		titular:       "Jefferson",
-		numeroAgencia: 589,
-		numeroConta:   123456,
-		saldo:         125.5,
-	}
+	contaCorrente1 := ContaCorrente{}
 
-	contaCorrente2 := ContaCorrente{
-		"José",
-		555,
-		111333,
-		200.0,
-	}
+	contaCorrente1.titular = "Jefferson"
+	contaCorrente1.saldo = 500
 
-	fmt.Println(contaCorrente1)
-	fmt.Println(contaCorrente2)
+	fmt.Println(contaCorrente1.saldo)
+
+	fmt.Println(contaCorrente1.Sacar(300))
+	fmt.Println(contaCorrente1.saldo)
+	fmt.Println(contaCorrente1.Sacar(600))
+	fmt.Println(contaCorrente1.Sacar(-100))
 }
